21d3dc78f9c50002e068e12c398fb4e2cc7e8401
Artem Kondratovich <artem.kndrtvch@gmail.com>
2022-01-31 13:09:41 +0300

    create db scripts

---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserProfiles.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vUserProfiles]
+AS 
+SELECT [UserProfiles].UserId,
+       ([UserProfiles].FirstName + ' ' + [UserProfiles].LastName) AS FullName,
+       [UserProfiles].Email,
+       [Directions].Name AS Direction,
+       [UserProfiles].Sex,
+       [UserProfiles].Education,
+       (SELECT DATEDIFF(HOUR, [UserProfiles].BirthDate, SYSDATETIME()) / 8760) AS Age,
+       [UserProfiles].UniversityAverageScore,
+       [UserProfiles].MathScore,
+       [UserProfiles].Address,
+       [UserProfiles].MobilePhone,
+       [UserProfiles].Skype,
+       [UserProfiles].StartDate
+FROM [UserProfiles]
+INNER JOIN [Directions] ON [UserProfiles].DirectionId = [Directions].DirectionId
---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserProgress.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vUserProgress]
+AS
+SELECT [UserTasks].UserId,
+       [UserTasks].TaskId,
+       [TaskTracks].TaskTrackId, 
+       [UserProfiles].FirstName + ' ' + [UserProfiles].LastName AS UserName,
+       [Tasks].Name AS TaskName,
+       [TaskTracks].TrackNote,
+       [TaskTracks].TrackDate
+FROM [UserTasks]
+INNER JOIN [Tasks] ON [UserTasks].TaskId = [Tasks].TaskId
+INNER JOIN [UserProfiles] ON [UserTasks].UserId = [UserProfiles].UserId
+INNER JOIN [TaskTracks] ON [UserTasks].UserTaskId = [TaskTracks].UserTaskId
