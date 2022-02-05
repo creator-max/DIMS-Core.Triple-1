4f0f5d93ce87b148d820e45681c83d60f6b81a81
Artem Kondratovich <artem.kndrtvch@gmail.com>
2022-02-05 10:47:12 +0300

    refactor: add unnecessary files to gitignor and refactor repositories

--- DIMS-Core/DIMS-Core.DataAccessLayer/Models/DIMSDatabaseContext.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Models/DIMSDatabaseContext.cs
@@ -18,15 +18,6 @@ namespace DIMS_Core.DataAccessLayer.Models
         public virtual DbSet<UserProfile> UserProfiles { get; set; }
         public virtual DbSet<VUserProfile> VUserProfiles { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-5UC1O79;Database=DIMSDatabase;Trusted_Connection=True;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Direction>(entity =>
--- DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/Base/ReadOnlyRepository.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/Base/ReadOnlyRepository.cs
@@ -1,13 +1,16 @@
 ﻿using DIMS_Core.DataAccessLayer.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DIMS_Core.DataAccessLayer.Repositories.Base
 {
     public abstract class ReadOnlyRepository<TEntity> : IReadOnlyRepository<TEntity>
         where TEntity : class
     {
+        protected readonly DbSet<TEntity> CurrentSet;
         private readonly DbContext _context;
+
         protected ReadOnlyRepository(DbContext context)
         {
             _context = context;
@@ -16,5 +19,11 @@ namespace DIMS_Core.DataAccessLayer.Repositories.Base
         public void Dispose() => _context?.Dispose();
 
         public IQueryable<TEntity> GetAll() => _context.Set<TEntity>().AsNoTracking();
+
+        public Task<TEntity> GetById(int id)
+        {
+            return CurrentSet.FindAsync(id)
+                             .AsTask();
+        }
     }
 }
