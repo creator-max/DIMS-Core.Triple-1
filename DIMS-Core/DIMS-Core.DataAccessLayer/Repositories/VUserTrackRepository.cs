ec531a4ef0b4fb9eea1397e232667bb5c8549d08
Maks Maksimeyko <maxamvlad@gmail.com>
2022-02-14 11:43:41 +0300

    Add repositories for Task, VTask, VUserTrack.

---
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VUserTrackRepository.cs
@@ -0,0 +1,14 @@
+﻿using DIMS_Core.DataAccessLayer.Context;
+using DIMS_Core.DataAccessLayer.Repositories.Base;
+using DIMS_Core.DataAccessLayer.Models;
+using DIMS_Core.DataAccessLayer.Interfaces;
+
+namespace DIMS_Core.DataAccessLayer.Repositories
+{
+    internal class VUserTrackRepository : Repository<VUserTrack>, IVUserTrackRepository
+    {
+        public VUserTrackRepository(DimsContext context) : base(context)
+        {
+        }
+    }
+}
